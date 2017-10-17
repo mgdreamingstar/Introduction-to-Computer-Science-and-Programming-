c30bd716cfb606a5d95e03d14686760eb6437a16
Guo Ruichen <guorch@buaa.edu.cn>
2017-10-17 22:05:23 +0800

    playHand()

--- Problem_Set_4/ps4a.py
+++ Problem_Set_4/ps4a.py
@@ -220,35 +220,35 @@ def playHand(hand, wordList, n):
       n: integer (HAND_SIZE; i.e., hand size required for additional points)
 
     """
-    # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
+    copy_hand = hand.copy()
     # Keep track of the total score
-
+    score = 0
     # As long as there are still letters left in the hand:
-
+    while sum(copy_hand.values()) != 0:
         # Display the hand
-
+        print 'Current Hand:', displayHand(copy_hand)
         # Ask user for input
-
+        enter = raw_input('Enter word, or a "." to indicate that you are finished:')
         # If the input is a single period:
-
+        if enter == '.':
             # End the game (break out of the loop)
-
-
+            break
         # Otherwise (the input is not a single period):
-
+        else:
             # If the word is not valid:
-
+            if not isValidWord(enter,copy_hand,wordList):
                 # Reject invalid word (print a message followed by a blank line)
-
+                print 'Invalid word, please try again.\n'
             # Otherwise (the word is valid):
-
+            else:
                 # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
-
+                score += getWordScore(enter,n)
+                print enter, 'earned', getWordScore(enter,n), 'points', ', and the total score is', score, '\n'
                 # Update the hand
-
+                copy_hand = updateHand(copy_hand,enter)
 
     # Game is over (user entered a '.' or ran out of letters), so tell user the total score
-
+    print 'Goodbye! Your total score is', score
 
 #
 # Problem #5: Playing a game
