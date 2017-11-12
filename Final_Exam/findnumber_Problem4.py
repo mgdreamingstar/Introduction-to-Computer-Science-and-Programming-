996bdff55bcd01ca4c50ae06e552eb67b9677def
Guo Ruichen <guorch@buaa.edu.cn>
2017-11-12 15:51:35 +0800

    Review 期末考试 Part 1

---
+++ Final_Exam/findnumber_Problem4.py
@@ -0,0 +1,55 @@
+# # 6.00.1x Final Exam
+# def secretNum(guess):
+#     secret = 10
+#     if guess > secret:
+#         return 1
+#     elif guess < secret:
+#         return -1
+#     elif guess == secret:
+#         return 0
+#
+# def findNum():
+#     guess = 1
+#     if secretNum(guess) == 0:
+#         return guess
+#     foundFlag = True
+#     while foundFlag:
+#         sign = secretNum(guess)
+#         if sign == -1:
+#             guess *= 2
+#         elif sign == 1:
+#             guess -= 1
+#         elif sign == 0:
+#             return guess
+#
+# print findNum()
+def isMyNumber(guess):
+    secret = 10
+    if guess > secret:
+        return 1
+    elif guess < secret:
+        return -1
+    elif guess == secret:
+        return 0
+
+def jumpAndBackpedal(isMyNumber):
+    '''
+    isMyNumber: Procedure that hides a secret number.
+     It takes as a parameter one number and returns:
+     *  -1 if the number is less than the secret number
+     *  0 if the number is equal to the secret number
+     *  1 if the number is greater than the secret number
+
+    returns: integer, the secret number
+    '''
+    guess = 1
+    if isMyNumber(guess) == 1:
+        return guess
+    foundNumber = False
+    while not foundNumber:
+        sign = isMyNumber(guess)
+        if sign == -1:
+            guess *= 2
+        else:
+            guess -= 1
+    return guess
