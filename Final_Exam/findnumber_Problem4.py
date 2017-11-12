c8153c6e03579def446808731399040636225c62
Guo Ruichen <guorch@buaa.edu.cn>
2017-11-12 16:03:42 +0800

    第一个判断的条件错了。

--- Final_Exam/findnumber_Problem4.py
+++ Final_Exam/findnumber_Problem4.py
@@ -24,7 +24,7 @@
 #
 # print findNum()
 def isMyNumber(guess):
-    secret = 10
+    secret = -24 # 10 原来只要大于的都没问题。因第一个条件判断 == 1 错了，应为 == 0.
     if guess > secret:
         return 1
     elif guess < secret:
@@ -43,13 +43,17 @@ def jumpAndBackpedal(isMyNumber):
     returns: integer, the secret number
     '''
     guess = 1
-    if isMyNumber(guess) == 1:
+    if isMyNumber(guess) == 0:
         return guess
     foundNumber = False
     while not foundNumber:
         sign = isMyNumber(guess)
         if sign == -1:
             guess *= 2
+        elif sign == 0:
+            foundNumber = True
         else:
             guess -= 1
     return guess
+
+print jumpAndBackpedal(isMyNumber)
